fc2f6b9f82d1c03032390aa2dad8c3f86330301b
user.email <vvshumilov@mail.ru>
2022-10-11 15:10:13 +0300

    fixed problems with user activity

--- app/src/main/java/com/hfad/mvvmgithub/main_viewmodel/adapter/AdapterUsers.kt
+++ app/src/main/java/com/hfad/mvvmgithub/main_viewmodel/adapter/AdapterUsers.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.hfad.mvvmgithub.databinding.ItemUsersBinding
 import com.hfad.mvvmgithub.model.Users
 import com.squareup.picasso.Picasso
 
